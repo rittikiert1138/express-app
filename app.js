04a334f443d5164edf0702f0d5c5fe45e880b917
Slottey <45114626+rittikiert1138@users.noreply.github.com>
2022-01-23 20:51:58 +0700

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res) => {
+    res.send('<h1>Welcome to express app 1.0</h1>')
+})
+
+app.get('/product1',(req,res) => {
+    res.send([
+        {
+            productId: '101',
+            productName: 'Yamaha'
+        },
+        {
+            productId: '102',
+            productName: 'Honda'
+        }
+    ])
+})
+
+app.listen(3000,() => console.log('App running on port 3000'))
